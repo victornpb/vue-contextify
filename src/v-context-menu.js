1e3eafc72879032608c7e9a65f5c65bb95a4dce7
victornpb <3372598+victornpb@users.noreply.github.com>
2022-04-25 23:51:00 -0300

    directive

--- src/v-context-menu.js
+++ src/v-context-menu.js
@@ -1,47 +1,49 @@
 
 import { triggerMenu } from './bridge.js';
 import { addLongPressListener, removeLongPressListener } from './longpress.js';
+const DEBUG = false;
+const LOG_PREFIX = '[VContextDirective]';
 
 const stateMap = new WeakMap(); // store nodes using the directive, HTMLDocument as key
 
 function newState(binding) {
   return {
     binding: binding, // directive binding object
-    isVisible: false, // current state of this Node (used for skipping doing unneeded work)
-    tooltipElm: undefined, // reference to the current tooltip element (lazy)
-    content: undefined, // the content of the tooltip (lazy)
-    margin: 5,
+    name: '',
   };
 }
 
 function eventHandler(e) {
-  console.log('[v-context-menu] eventHandler', e);
+  if (DEBUG) console.log(LOG_PREFIX, 'eventHandler', e);
   e.preventDefault();
   const state = stateMap.get(this);
 
-  // name of the menu, as modifier
-  const name = Object.keys(state.binding.modifiers)[0];
-
-  triggerMenu(name, state.binding.value, e);
+  // trigger the menu
+  triggerMenu(state.name, state.binding.value, e);
 }
-
 
 export default {
   bind(el, binding) {
-    console.log('[v-context-menu] bind directive', el, binding);
-    stateMap.set(el, newState(binding));
+    if (DEBUG) console.log(LOG_PREFIX, 'bind directive', el, binding);
+    const state = newState(binding);
+    stateMap.set(el, state);
+
+    // name of the menu, as modifier
+    const name = Object.keys(binding.modifiers)[0];
+    if (name === undefined) throw new Error(LOG_PREFIX + ' You need to provide a name, as modifier!');
+    state.name = name;
 
     el.addEventListener('contextmenu', eventHandler);
     addLongPressListener(el, eventHandler);
   },
   update(el, binding) {
-    console.log('[v-context-menu] update directive', el, binding);
-    const state =  stateMap.get(el);
+    if (DEBUG) console.log(LOG_PREFIX, 'update directive', el, binding);
+    const state = stateMap.get(el);
     state.binding = binding;
   },
   unbind(el, _binding) {
-    console.log('[v-context-menu] unbind directive', el, _binding);
-    const state = stateMap.get(el);
+    if (DEBUG) console.log(LOG_PREFIX, 'unbind directive', el, _binding);
+    // const state = stateMap.get(el);
     stateMap.delete(el);
 
     el.removeEventListener('contextmenu', eventHandler);
